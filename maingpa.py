109cce5de238cf66eadfc064530bdbea419d7968
Matsebula007 <101245930+Matsebula007@users.noreply.github.com>
2022-11-16 13:04:15 +0200

    readind and writing to database

--- maingpa.py
+++ maingpa.py
@@ -2,8 +2,10 @@ from kivy.core.window import Window
 from kivy.lang import Builder
 from kivymd.app import MDApp
 from kivymd.uix.screen import MDScreen
+import json
 
 
+'''
 class HomePage(MDScreen):
     pass
 
@@ -14,6 +16,28 @@ class MainApp(MDApp):
         Builder.load_file('ScreensDesign.kv')
         return HomePage()
 
+'''
 
 if __name__ == "__main__":
-    MainApp().run()
+    #MainApp().run()
+    #read database and pass as string    
+    database = open('assets/course_database.json','r')
+    data = database.read()
+    #pass string as dictionary
+    datar =json.loads(data)
+    test1name = datar['sheets'][0]['lines'][0]['course_ass'][0]['ass_number'][0]['id']
+    
+
+    newmark = float(input("Enter mark : "))
+    datar['sheets'][0]['lines'][0]['course_ass'][0]['ass_number'][0]['Mark'] = newmark
+    database = open('assets/course_database.json','w')
+    json.dump(datar, database)
+    test1mar = datar['sheets'][0]['lines'][0]['course_ass'][0]['ass_number'][0]['Mark']
+    database.close()
+    print( test1name ," : ", test1mar)
+    
+
+
+    
+
+
