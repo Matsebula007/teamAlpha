3cfea73c641160a9ddc0970eeb212926005e8993
Matsebula007 <101245930+Matsebula007@users.noreply.github.com>
2022-11-22 21:07:11 +0200

    login authentication

--- maingpa.py
+++ maingpa.py
@@ -26,10 +26,10 @@ class MainApp(MDApp):
         global screen_manager
         self.title ="Ä Goals"
         screen_manager = ScreenManager()
-        #screen_manager.add_widget(Builder.load_file("Screens/Main.kv"))
-        #screen_manager.add_widget(Builder.load_file("Screens/Login.kv"))
-        #screen_manager.add_widget(Builder.load_file("Screens/SignUp.kv"))
-        #screen_manager.add_widget(Builder.load_file('Screens/PageScreens.kv'))
+        screen_manager.add_widget(Builder.load_file("Screens/Main.kv"))
+        screen_manager.add_widget(Builder.load_file("Screens/Login.kv"))
+        screen_manager.add_widget(Builder.load_file("Screens/SignUp.kv"))
+        screen_manager.add_widget(Builder.load_file('Screens/PageScreens.kv'))
         screen_manager.add_widget(Builder.load_file('Screens/Addtodo.kv'))
         screen_manager.add_widget(Builder.load_file('Screens/todo.kv'))
         
@@ -83,14 +83,85 @@ class MainApp(MDApp):
                     font_size ="19dp").open()
 
 
+    def userSignUp(self,user_name,user_email,user_password):
+        usr_database = open('assets/user_data.json','r')
+        data = usr_database.read()
+        usr_datar = json.loads(data)
 
+        user_name = screen_manager.get_screen("SignUp").usr_name.text
+        user_email = screen_manager.get_screen("SignUp").usr_email.text
+        user_password = screen_manager.get_screen("SignUp").usr_pass.text
+        
+       
+        #m-put in own conditional branch
+        if user_name !="" and user_email !="" and user_password !="" and len(user_name)<21 and len(user_password)<60:
+            usr_datar['sheets'][0]['lines'][0]['usr_name'] = user_name
+            usr_datar['sheets'][0]['lines'][0]['usr_email'] = user_email
+            usr_datar['sheets'][0]['lines'][0]['usr_password']= user_password
+            user_dbase =open('assets/user_data.json','w')
+            
+            json.dump(usr_datar,user_dbase,indent=4)
+            screen_manager.transition.direction = "right"
+            screen_manager.current = "Home"
+
+        elif user_name =="":
+            Snackbar(text="Username  missing!",snackbar_x ="10dp",snackbar_y ="10dp",
+                    size_hint_x =(Window.width -(dp(10)*2))/Window.width, bg_color=(1,170/255,23/255,1),
+                    font_size ="19dp").open()
+        elif user_email =="":
+            Snackbar(text="Email  missing!",snackbar_x ="10dp",snackbar_y ="10dp",
+                    size_hint_x =(Window.width -(dp(10)*2))/Window.width, bg_color=(1,170/255,23/255,1),
+                    font_size ="19dp").open()
+        elif user_password =="":
+            Snackbar(text="Password missing!",snackbar_x ="10dp",snackbar_y ="10dp",
+                    size_hint_x =(Window.width -(dp(10)*2))/Window.width, bg_color=(1,170/255,23/255,1),
+                    font_size ="19dp").open()
+        elif len(user_name)>21:
+            Snackbar(text="Username must<21",snackbar_x ="10dp",snackbar_y ="10dp",
+                    size_hint_x =(Window.width -(dp(10)*2))/Window.width, bg_color=(1,170/255,23/255,1),
+                    font_size ="19dp").open()
+        elif len(user_password)>61 :
+            Snackbar(text="Password must<61",snackbar_x ="10dp",snackbar_y ="10dp",
+                    size_hint_x =(Window.width -(dp(10)*2))/Window.width, bg_color=(1,170/255,23/255,1),
+                    font_size ="19dp").open()
+
+    def userlogin(self,user_name,user_password):
+        usr_database = open('assets/user_data.json','r')
+        data = usr_database.read()
+        usr_datar = json.loads(data)
+            
+        user_name = screen_manager.get_screen("Login").usr_Username.text
+        user_password = screen_manager.get_screen("Login").usr_password.text
+        
+        usname = usr_datar['sheets'][0]['lines'][0]['usr_name'] 
+        uspassword = usr_datar['sheets'][0]['lines'][0]['usr_password']
+
+        if user_name !="" and user_password !="" and user_name == usname and user_password == uspassword:
+            screen_manager.transition.direction = "right"
+            screen_manager.current = "Home"
+
+        elif user_name =="":
+            Snackbar(text="Username  missing!",snackbar_x ="10dp",snackbar_y ="10dp",
+                    size_hint_x =(Window.width -(dp(10)*2))/Window.width, bg_color=(1,170/255,23/255,1),
+                    font_size ="19dp").open()
+        elif user_password =="":
+            Snackbar(text="Password missing!",snackbar_x ="10dp",snackbar_y ="10dp",
+                    size_hint_x =(Window.width -(dp(10)*2))/Window.width, bg_color=(1,170/255,23/255,1),
+                    font_size ="19dp").open()
+        elif user_name != usname:
+            Snackbar(text="Invalid username",snackbar_x ="10dp",snackbar_y ="10dp",
+                    size_hint_x =(Window.width -(dp(10)*2))/Window.width, bg_color=(1,170/255,23/255,1),
+                    font_size ="19dp").open()
+        elif user_password != uspassword:
+            Snackbar(text="Invalid password",snackbar_x ="10dp",snackbar_y ="10dp",
+                    size_hint_x =(Window.width -(dp(10)*2))/Window.width, bg_color=(1,170/255,23/255,1),
+                    font_size ="19dp").open()
 
     #m-add call append to database funct 
 
 
 
-
-    def stringSearch(self,stringTosearch ):
+"""  def stringSearch(self,stringTosearch ):
         # Input the key value that you want to search
         stringTosearch = input("Enter a key value: \n")
         # load the json data
@@ -107,11 +178,9 @@ class MainApp(MDApp):
             # Print the message if the value does not exist
             print("%s is not found in JSON data" %stringTosearch)
     def userrSignUp(self,usr_name,usr_email,usr_password):
-        pass
+        pass """
 
-        
-
-        
+    
 
 
 if __name__ == "__main__":
